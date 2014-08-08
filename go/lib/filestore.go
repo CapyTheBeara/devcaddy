07eca8895556141ca1d3f6f4d402a7d7911aaa1a
monocle <hchw29@gmail.com>
2014-08-07 20:54:37 -0700

    Filestore - has input channel to process files. add methods that return files

--- go/lib/filestore.go
+++ go/lib/filestore.go
@@ -1,15 +1,33 @@
 package lib
 
 import (
+	"path/filepath"
 	"sort"
+	"strings"
 )
+
+func NewStore(c *Config) *Store {
+	store = &Store{
+		Root:      c.Root,
+		Files:     make(map[string]*File),
+		Input:     make(chan *File),
+		DidUpdate: make(chan *File),
+	}
+
+	for _, f := range c.Files {
+		store.Files[f.Name] = f
+	}
+
+	return store
+}
 
 var store *Store
 
 type Store struct {
 	Root      string
 	Files     map[string]*File
-	DidUpdate chan string
+	Input     chan *File
+	DidUpdate chan *File // TODO - rename to Output
 }
 
 func (s *Store) Put(name string, content string, args ...bool) {
@@ -22,58 +40,108 @@ func (s *Store) Put(name string, content string, args ...bool) {
 	}
 
 	if update {
-		s.doUpdate(name)
+		s.doUpdate(f)
 	}
 }
 
+func (s *Store) PutFile(f *File) {
+	s.Files[f.Name] = f
+	s.doUpdate(f)
+}
+
 func (s *Store) Get(name string) string {
-	f := s.Files[name]
+	f := s.GetFile(name)
 	if f == nil {
 		return ""
 	}
+	return f.Content
+}
 
-	if f.Type == "merge" {
-		return f.MergeStoreFiles(s)
+func (s *Store) GetFile(name string) *File {
+	f := s.Files[name]
+	if f == nil {
+		return nil
 	}
 
-	return f.Content
+	if f.Type == "merge" {
+		s.MergeStoreFiles(f)
+	}
+
+	return f
 }
 
 func (s *Store) Delete(name string) {
 	delete(s.Files, name)
-	s.doUpdate(name)
+	s.doUpdate(&File{Name: name})
+}
+
+func (s *Store) DeleteFile(f *File) {
+	delete(s.Files, f.Name)
+	s.doUpdate(f)
 }
 
 func (s *Store) GetAll() (files []*File) {
-	names := []string{}
-	for name, _ := range s.Files {
-		names = append(names, name)
-	}
-
-	sort.Strings(names)
-
-	for _, n := range names {
-		files = append(files, s.Files[n])
+	for _, n := range s.SortedFileNames() {
+		files = append(files, s.GetFile(n))
 	}
 	return
 }
 
-func (s *Store) doUpdate(name string) {
+func (s *Store) SortedFileNames() []string {
+	names := []string{}
+	for name, _ := range s.Files {
+		names = append(names, name)
+	}
+	sort.Strings(names)
+	return names
+}
+
+func (s *Store) Listen() {
 	go func() {
-		s.DidUpdate <- name
+		for {
+			f := <-s.Input
+			if f == nil {
+				continue
+			}
+
+			switch f.Op {
+			case CREATE:
+				s.PutFile(f)
+			case WRITE:
+				s.PutFile(f)
+			case REMOVE:
+				s.DeleteFile(f)
+			case RENAME:
+				s.DeleteFile(f)
+			}
+		}
 	}()
 }
 
-func NewStore(root string, config *Config) *Store {
-	store = &Store{
-		Root:      root,
-		Files:     make(map[string]*File),
-		DidUpdate: make(chan string),
+func (s *Store) MergeStoreFiles(file *File) string {
+	contents := []string{}
+	dir := filepath.Join(s.Root, file.Dir)
+
+	if len(file.Files) > 0 {
+		for _, f := range file.Files {
+			path := filepath.Join(dir, f)
+			contents = append(contents, s.Get(path))
+		}
+	} else {
+		for _, n := range s.SortedFileNames() {
+			f := s.Files[n]
+			if strings.Contains(f.Name, dir) && strings.HasSuffix(f.Name, "."+file.Ext) {
+				contents = append(contents, f.Content)
+			}
+		}
 	}
 
-	for _, f := range config.Files {
-		store.Files[f.Name] = f
-	}
+	file.Content = strings.Join(contents, "\n")
+	return file.Content
+}
 
-	return store
+func (s *Store) doUpdate(f *File) {
+	go func() {
+		s.DidUpdate <- f
+	}()
 }
--- go/lib/filestore_test.go
+++ go/lib/filestore_test.go
@@ -2,13 +2,14 @@ package lib
 
 import (
 	"testing"
+	"time"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 const cfg = `
 {
-    "root": "",
+    "root": "/proj",
     "files": [
         {
             "name": "app.js",
@@ -34,7 +35,7 @@ const cfg = `
 
 func TestFileAccessing(t *testing.T) {
 	Convey("Given files exist in the store", t, func() {
-		store := NewStore("/proj", NewConfig([]byte(cfg)))
+		store := NewStore(NewConfig([]byte(cfg)))
 		store.Put("/proj/app/controllers/foo.js", "foo")
 		store.Put("/proj/app/models/bar.js", "bar")
 		store.Put("/proj/app/routes/baz.js", "baz")
@@ -45,16 +46,16 @@ func TestFileAccessing(t *testing.T) {
 		store.Put("/proj/vendor/bax/index.js", "bax")
 		store.Put("/proj/vendor/qux/qux.css", "qstyle")
 
-		Convey("A file can be retrieved", func() {
+		Convey("A file's content can be retrieved", func() {
 			So(store.Get("/proj/app/controllers/foo.js"), ShouldEqual, "foo")
 		})
 
-		Convey("A file can be updated", func() {
+		Convey("A file's content can be updated", func() {
 			store.Put("/proj/app/controllers/foo.js", "zzz")
 			So(store.Get("/proj/app/controllers/foo.js"), ShouldEqual, "zzz")
 		})
 
-		Convey("A file can be deleted", func() {
+		Convey("A file's content can be deleted", func() {
 			store.Delete("/proj/app/routes/baz.js")
 			So(store.Get("/proj/app/routes/baz.js"), ShouldEqual, "")
 		})
@@ -72,6 +73,12 @@ func TestFileAccessing(t *testing.T) {
 			Convey("It has the correct content after adding a file", func() {
 				store.Put("/proj/app/controllers/app.js", "app")
 				So(store.Get("app.js"), ShouldEqual, "app\nfoo\nbar\nbaz")
+			})
+
+			Convey("GetAll will include content for merge files", func() {
+				f := store.GetAll()[10]
+				So(f.Name, ShouldEqual, "app.js")
+				So(f.Content, ShouldEqual, "foo\nbar\nbaz")
 			})
 		})
 
@@ -92,21 +99,127 @@ func TestFileAccessing(t *testing.T) {
 	})
 }
 
+func TestFileAccessors(t *testing.T) {
+	Convey("Given files exist in the store", t, func() {
+		store := NewStore(NewConfig([]byte(cfg)))
+		store.Put("/proj/app/controllers/foo.js", "foo")
+		store.Put("/proj/app/models/bar.js", "bar")
+		store.Put("/proj/app/routes/baz.js", "baz")
+		store.Put("/proj/app/styles/app.css", "css")
+		store.Put("/proj/app/styles/foo.css", "foocss")
+		store.Put("/proj/app/templates/app.hbs", "hello")
+		store.Put("/proj/vendor/qux/main.js", "qux")
+		store.Put("/proj/vendor/bax/index.js", "bax")
+		store.Put("/proj/vendor/qux/qux.css", "qstyle")
+
+		Convey("A file can be retrieved", func() {
+			f := store.GetFile("/proj/app/controllers/foo.js")
+			So(f.Name, ShouldEqual, "/proj/app/controllers/foo.js")
+			So(f.Content, ShouldEqual, "foo")
+		})
+
+		Convey("A defined file has the correct content", func() {
+			f := store.GetFile("app.js")
+			So(f.Name, ShouldEqual, "app.js")
+			So(f.Type, ShouldEqual, "merge")
+			So(f.Content, ShouldEqual, "foo\nbar\nbaz")
+		})
+
+	})
+}
+
 func TestUpdatedChannel(t *testing.T) {
 	Convey("Given a file was added", t, func() {
-		store := NewStore("/proj", NewConfig([]byte(cfg)))
+		store := NewStore(NewConfig([]byte(cfg)))
 		store.Put("/proj/app/controllers/foo.js", "foo")
 		res := <-store.DidUpdate
 
 		Convey("An update was triggered", func() {
-			So(res, ShouldEqual, "/proj/app/controllers/foo.js")
+			So(res.Name, ShouldEqual, "/proj/app/controllers/foo.js")
 		})
 
 		Convey("Deleting the file triggers an update", func() {
 			store.Delete("/proj/app/controllers/foo.js")
 
 			res = <-store.DidUpdate
-			So(res, ShouldEqual, "/proj/app/controllers/foo.js")
+			So(res.Name, ShouldEqual, "/proj/app/controllers/foo.js")
+		})
+	})
+}
+
+func TestStoreListen(t *testing.T) {
+	Convey("Given a store is listening for files", t, func() {
+		s := NewStore(NewConfig([]byte(cfg)))
+		s.Listen()
+
+		Convey("It handles file create", func() {
+			s.Input <- NewFileWithContent("/proj/app/controllers/foo.js", "foo", CREATE)
+			f := <-store.DidUpdate
+
+			So(f.Name, ShouldEqual, "/proj/app/controllers/foo.js")
+			So(f.Op, ShouldEqual, CREATE)
+		})
+
+		Convey("It handles file write", func() {
+			s.Input <- NewFileWithContent("/proj/app/controllers/1.js", "1", WRITE)
+			f := <-store.DidUpdate
+
+			So(f.Name, ShouldEqual, "/proj/app/controllers/1.js")
+			So(f.Op, ShouldEqual, WRITE)
+		})
+
+		Convey("It handles file removal", func() {
+			s.Input <- NewFileWithContent("/proj/app/controllers/2.js", "", REMOVE)
+			f := <-store.DidUpdate
+
+			So(store.GetFile("/proj/app/controllers/2.js"), ShouldBeNil)
+			So(f.Name, ShouldEqual, "/proj/app/controllers/2.js")
+			So(f.Op, ShouldEqual, REMOVE)
+		})
+
+		Convey("It handles file rename", func() {
+			s.Input <- NewFileWithContent("/proj/app/controllers/3.js", "", RENAME)
+			f := <-store.DidUpdate
+
+			So(store.GetFile("/proj/app/controllers/3.js"), ShouldBeNil)
+			So(f.Name, ShouldEqual, "/proj/app/controllers/3.js")
+			So(f.Op, ShouldEqual, RENAME)
+		})
+
+		Convey("It ignores other file modes", func() {
+			s.Input <- NewFileWithContent("/proj/app/controllers/4.js", "", CHMOD)
+
+			select {
+			case f := <-store.DidUpdate:
+				So("Fail - Store should not update", ShouldEqual, f)
+			default:
+				So("Pass - Store did not update", ShouldNotBeBlank)
+			}
+		})
+
+		Convey("It ignores nil files", func() {
+			var f *File
+			s.Input <- f
+			time.Sleep(time.Millisecond * 30)
+
+			select {
+			case f := <-store.DidUpdate:
+				So("Fail - Store should not update", ShouldEqual, f)
+			default:
+				So("Pass - Store did not update", ShouldNotBeBlank)
+			}
+		})
+
+		Convey("It ignores LOG files", func() {
+			s.Input <- &File{Name: "foo", Op: LOG}
+			time.Sleep(time.Millisecond * 30)
+
+			select {
+			case f := <-store.DidUpdate:
+				So("Fail - Store should not update", ShouldEqual, f)
+			default:
+				So("Pass - Store did not update", ShouldNotBeBlank)
+			}
 		})
 	})
 }
