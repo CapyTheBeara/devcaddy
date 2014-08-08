72c7680e9fc997bdcc82fbeb92a35114784e88bd
monocle <hchw29@gmail.com>
2014-08-07 21:07:32 -0700

    add main()

--- go/devcaddy.go
+++ go/devcaddy.go
@@ -5,26 +5,34 @@ import (
 	"log"
 	"strconv"
 
-	"github.com/monocle/devcaddy/lib"
+	"github.com/monocle/devcaddy/devcaddy/lib"
 )
 
 func main() {
-	log.SetFlags(log.Ltime)
+	log.SetFlags(log.Ltime | log.Lshortfile)
 	lib.Plog = true
 
 	done := make(chan bool)
+	watcherOutput := make(chan *lib.File)
 
 	cfg, err := ioutil.ReadFile("devcaddy.json")
 	if err != nil {
-		log.Fatalln("[error] Problem reading devcaddy_config.json", err)
+		log.Fatalln("[error] Problem reading devcaddy.json", err)
 	}
-
 	c := lib.NewConfig(cfg)
-	store := c.PopulateStore(done)
+
+	plugins := lib.NewPlugins(c.PluginConfs)
+	// TODO remove this
+	c.Plugins = plugins
+
+	watchers := lib.NewWatchers(c, watcherOutput)
+	size := watchers.GetInitialFiles()
+
+	store := lib.NewStore(c)
+	store.Input = lib.LogProcessedFiles(watcherOutput, done, size)
+	store.Listen()
 
 	<-done
-
 	lib.Plog.PrintC("server", strconv.Itoa(len(store.Files))+" files defined in store")
 	lib.StartServer(store, "4200", "", "assets")
-
 }
